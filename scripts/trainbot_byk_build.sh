008834a8a95dbbd55a10d137aa2dfecb49cf26fd
KlviG <78801020+KlviG@users.noreply.github.com>
2023-04-05 11:38:27 +0300

    Update trainbot_byk_build.sh

--- scripts/trainbot_byk_build.sh
+++ scripts/trainbot_byk_build.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-#check prerequisites
-command -v python3 >/dev/null 2>&1 || { echo >&2 "python is required but it's not installed. "; echo "Installation guide: https://docs.python-guide.org/starting/install3/linux/"; prereq="null"; } && command -v docker >/dev/null 2>&1 || { echo >&2 "docker is required but it's not installed. "; echo "Installation guide: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04"; prereq="null"; } && command -v docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed. "; echo "Installation guide: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-20-04"; prereq="null"; }
-if [ "$prereq" == "null" ]; then
-    echo "Aborting"; exit 1
+echo -e "[+] \x1b[1;32mcheck prerequisites\x1b[0m"
+for cmd in docker docker-compose git; do
+    if ( ! which "$cmd" > /dev/null 2>&1 ); then
+        echo -e "[+] \x1b[1;33mCommand '$cmd' is required, but not installed! Aborting.\x1b[0m"
+        exit 1
+    else
+        echo -e "[+] Command '$cmd' is installed"
+    fi
+done
+
+echo -e "[+] \x1b[1;32mimport variables from config\x1b[0m"
+if [ -f "config.sh" ]; then
+    source config.sh
+else
+    echo -e "[+] \x1b[1;33mConfiguration file not found. Make sure you have configured all values in the specified configuration file.\x1b[0m"
+    exit 1
 fi
-#show path of config
-config="/path/to/config.txt"
-#import $vars from config
-buildpath=$(sed 's/buildpath=//g;9q;d' $config)
-bot_name=$(sed 's/keytoolCN=//g;13q;d' $config)
-BOT_HOST=$(sed 's/keytoolOU=//g;14q;d' $config)
-BOT_USER=$(sed 's/username=//g;6q;d' $config)
-SSH_KEY=$(sed 's/SSH_KEY=//g;23q;d' $config)
-BOT_DATA_DIR=$(sed 's/BOT_DATA_DIR=//g;24q;d' $config)
+
 cd $buildpath
 git clone https://github.com/buerokratt/Installation-Guides.git
 cd Installation-Guides
@@ -21,11 +25,13 @@ deploy="$buildpath/Installation-Guides/default-setup/chatbot-and-training/bot_tr
 train="$buildpath/Installation-Guides/default-setup/chatbot-and-training/bot_training/train.sh"
 echo "$(sed "s|DIR=/your/actual/directory|DIR=$buildpath/Installation-Guides/default-setup/chatbot-and-training/bot/loba|g" $train)" > $train
 echo "$(sed "s|DIR=/your/actual/directory|DIR=$buildpath/Installation-Guides/default-setup/chatbot-and-training/bot/loba|g" $deploy)" > $deploy
-echo "$(sed "s|BOT_HOST='BOT_HOSTNAME'|BOT_HOST='$BOT_HOST'|g" $deploy)" > $deploy
-echo "$(sed "s|BOT_USER='BOT_USER'|BOT_USER='$BOT_USER'|g" $deploy)" > $deploy
+echo "$(sed "s|BOT_HOST='BOT_HOSTNAME'|BOT_HOST='$keytoolOU'|g" $deploy)" > $deploy
+echo "$(sed "s|BOT_USER='BOT_USER'|BOT_USER='$username'|g" $deploy)" > $deploy
 echo "$(sed "s|SSH_KEY='SSH_KEY_PATH'|SSH_KEY='$SSH_KEY'|g" $deploy)" > $deploy
 
 cd $buildpath/Installation-Guides/default-setup/chatbot-and-training/bot_training/
 mv * $buildpath/../
-cd
+cd $buildpath
+chmod +x deploy.sh train.sh
+docker-compose up -d
 exec bash
