435067a65af804f83caca4aefc2bdd1b204d663f
KlviG <78801020+KlviG@users.noreply.github.com>
2023-04-05 11:44:17 +0300

    Update bot_byk_build.sh

--- scripts/bot_byk_build.sh
+++ scripts/bot_byk_build.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-#check prerequisites
-command -v python3 >/dev/null 2>&1 || { echo >&2 "python is required but it's not installed. "; echo "Installation guide: https://docs.python-guide.org/starting/install3/linux/"; prereq="null"; } && command -v docker >/dev/null 2>&1 || { echo >&2 "docker is required but it's not installed. "; echo "Installation guide: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04"; prereq="null"; } && command -v docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed. "; echo "Installation guide: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-20-04"; prereq="null"; }
-if [ "$prereq" == "null" ]; then
-    echo "Aborting"; exit 1
+echo -e "[+] \x1b[1;32mcheck prerequisites\x1b[0m"
+for cmd in docker docker-compose git; do
+    if ( ! which "$cmd" > /dev/null 2>&1 ); then
+        echo -e "[+] \x1b[1;33mCommand '$cmd' is required, but not installed! Aborting.\x1b[0m"
+        exit 1
+    else
+        echo -e "[+] Command '$cmd' is installed"
+    fi
+done
+
+echo -e "[+] \x1b[1;32mimport variables from config\x1b[0m"
+if [ -f "config.sh" ]; then
+    source config.sh
+else
+    echo -e "[+] \x1b[1;33mConfiguration file not found. Make sure you have configured all values in the specified configuration file.\x1b[0m"
+    exit 1
 fi
-#show path of config
-config="/path/to/config.txt"
-#import $vars from config
-buildpath=$(sed 's/buildpath=//g;9q;d' $config)
 cd $buildpath
 git clone https://github.com/buerokratt/Installation-Guides.git
 cd Installation-Guides
