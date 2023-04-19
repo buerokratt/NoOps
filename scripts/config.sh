207734002191740972d01f7d9f5fa5f1bf6794b4
KlviG <78801020+KlviG@users.noreply.github.com>
2023-04-19 13:12:13 +0300

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
-organization=test
+organization=ORGANISATION
 username=ubuntu
-safe_tim_db=
-safe_byk_db=
+safe_tim_db=PASSWORD(min 6 characters)
+safe_byk_db=PASSWORD(min 6 characters)
 buildpath="$(pwd)"
-timdb=
-taraid=
-tarapass=
-keytoolpass=
-bot_url=
-training_url=
-training_bot_dir=
-SSH_KEY=
-BOT_DATA_DIR=
-bot_name
+timdb=DATABASE-VM-IP
+taraid=GIVEN-FROM-TARA-ENVELOPE
+tarapass=GIVEN-FROM-TARA-ENVELOPE
+keytoolpass=PASSWORD(min 6 characters)
+bot_url=BOT-VM-IP
+training_url=TRAININGBOT-VM-IP
+training_bot_dir=chatbot
+SSH_KEY=PATH-TO-SSH-KEY(id_rsa)
+BOT_DATA_DIR=PATH-TO-BOT-DATA
+bot_name=ORGANISATION-NAME-FOR-BOT
