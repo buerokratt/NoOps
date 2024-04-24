b4bb94ba702f5fdfacf92d5cbaa6f92936ef4379
varmoh <oldviking@gmail.com>
2024-04-24 22:36:49 +0300

    Update 2 - Removed obsolete folder and yaml inside the "first-login" deployment

--- Kubernetes/Post-deploy/first-login/script/init.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO public.user (login, first_name, last_name, id_code, display_name, status, created, csa_title, csa_email)
-VALUES ('EE30303039914', 'OK', 'TESTNUMBER', 'EE30303039914', 'OK TESTNUMBER', 'active', now(), 'OG2', 'og@mail.ee');
-
-INSERT INTO user_authority (user_id, authority_name, created)
-VALUES ((SELECT id_code FROM public.user WHERE login = 'EE30303039914'), ARRAY['ROLE_ADMINISTRATOR'], now());
