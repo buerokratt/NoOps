497d02f3a41bb0871ad083d37e5837e866c42beb
KlviG <78801020+KlviG@users.noreply.github.com>
2025-03-31 12:35:27 +0300

    Update update-image.sh

--- Cron/scripts/update-image.sh
+++ Cron/scripts/update-image.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo $1
 
-echo "print version"
+if [[ $1 == *component_bot_image_version* ]]; then
+  sed -i "s|component_bot_image_version=[^ ]*|$1|g" MasterSecrets.env
+else
+  echo "did not get input"
 
-return 200
+fi
+
+echo done
